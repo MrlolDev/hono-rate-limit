bb78215033127597a881acf5639e8822171d7a38
MrlolDev <65485999+MrlolDev@users.noreply.github.com>
2024-03-08 18:51:38 +0000

    Add rate limit middleware and support for Redis storage

--- README.md
+++ README.md
@@ -7,6 +7,45 @@ npm i hono-rate-limit
 ```
 
 ## Usage
+### Import dependencies
+```typescript
+import { Hono } from 'hono'
+import RateLimit from 'hono-rate-limit'
+```
+
+### Configure your rate limit
+#### Local storage
+```typescript
+const honoRateLimit = new RateLimit({
+    windowMs: 2000,
+    limit: 1, 
+    store: "local",
+    message:  "Too many requests", // just text supported
+    statusCode: 429,
+})
+```
+#### Redis storage
+```typescript
+const honoRateLimit = new RateLimit({
+    windowMs: 2000,
+    limit: 1, 
+    store: "redis",
+    message:  "Too many requests", // just text supported
+    statusCode: 429,
+    redisOptions: {
+        url: 'redis://alice:foobared@awesome.redis.server:6380'
+    }
+})
+```
+
+### Set up the rate limit middleware
+```typescript
+const app = new Hono()
+
+app.use('/*', honoRateLimit.middleware())
+```
+
+## Full-code examples
 ```typescript
 import { Hono } from 'hono'
 import RateLimit from 'hono-rate-limit'
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
 import { Context } from "hono";
 import { StatusCode } from "hono/utils/http-status";
+import redis from "redis";
 
 export default class RateLimit {
   windowMs: number;
   limit: number;
-  store: Map<any, any>;
+  store:
+    | Map<any, any>
+    | {
+        get: (key: string) => Promise<any>;
+        set: (key: string, value: any) => Promise<void>;
+        delete: (key: string) => Promise<void>;
+      };
   message: string;
   statusCode: number;
   constructor(options: {
     windowMs?: number;
     limit?: number;
-    store?: "local";
+    store?: "local" | "redis";
+    redisOptions?: redis.RedisClientOptions;
     message?: string;
     statusCode?: number;
   }) {
@@ -22,6 +30,27 @@ export default class RateLimit {
     if (options.store === "local") {
       this.store = new Map();
     }
+    if (options.store === "redis") {
+      const redisClient = redis.createClient(options.redisOptions);
+      this.store = {
+        get: async (key: string) => {
+          let record = await redisClient.get(key);
+          if (record) {
+            return JSON.parse(record);
+          }
+        },
+        set: async (key: string, value: any) => {
+          await redisClient.setEx(
+            key,
+            this.windowMs / 1000,
+            JSON.stringify(value)
+          );
+        },
+        delete: async (key: string) => {
+          await redisClient.del(key);
+        },
+      };
+    }
     if (!options.message) options.message = "Too many requests";
     if (!options.statusCode) options.statusCode = 429;
     this.message = options.message;
@@ -32,29 +61,33 @@ export default class RateLimit {
       c.req.header("x-forwarded-for")?.split(",")[0].trim() ||
       "IP not available";
     const now = Date.now();
-    let record = this.store.get(ip);
+    let record = await this.store.get(ip);
     if (!record) {
       record = {
         count: 1,
         resetTime: now + this.windowMs,
       };
-      this.store.set(ip, record);
+      await this.store.set(ip, record);
       await next();
     } else {
-      if (record.count >= this.limit) {
+      if (now > record.resetTime) {
+        record.count = 1;
+        record.resetTime = now + this.windowMs;
+        await this.store.set(ip, record);
+        await next();
+      } else if (record.count >= this.limit) {
         return c.text(this.message, this.statusCode as StatusCode);
+      } else {
+        record.count++;
+        await this.store.set(ip, record);
+        await next();
       }
-      record.count++;
-      this.store.set(ip, record);
     }
   };
-  getIpUsage = (ip: string) => {
-    return this.store.get(ip);
-  }
-  deleteIpUsage = (ip: string) => {
-    this.store.delete(ip);
-  }
-  clear = () => {
-    this.store.clear();
-  }
+  getIpUsage = async (ip: string) => {
+    return await this.store.get(ip);
+  };
+  deleteIpUsage = async (ip: string) => {
+    await this.store.delete(ip);
+  };
 }
